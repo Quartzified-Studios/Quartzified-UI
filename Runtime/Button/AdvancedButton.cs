fa288fc9f3acd26903e9dbf8bf29f5acf4657885
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-07-29 22:47:13 +0200

    Added New UI Button variants

---
+++ Runtime/Button/AdvancedButton.cs
@@ -0,0 +1,260 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using Quartzified.Audio;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using TMPro;
+#endif
+
+namespace Quartzified.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class AdvancedButton : Selectable, IPointerClickHandler
+    {
+        [Header("Visual")]
+        public Sprite enabledSprite;
+        public Sprite enabledSpritePressed;
+        [Space]
+        public Sprite disabledSprite;
+        public Sprite disabledSpritePressed;
+
+        [Header("Tap & Hold")]
+        public bool tapAndHold;
+        public float initialTapDelay = 0;
+        public float holdDelay = 0.5f;
+        public float fastTapDelay = 0.2f;
+        public float fastTapHoldDelay = 0.1f;
+
+        [Header("Sound Effects")]
+        public AudioClip enabledSound;
+        public AudioClip disabledSound;
+        public float soundEffectCooldown = .1f;
+        float sfxCooldown;
+
+        [Header("Events")]
+        public UnityEvent OnClicked;
+        public UnityEvent OnDisabledClicked;
+        
+        // Enabled Click or Disabled Click : True / False
+        public bool interactState = true;
+
+        #region Tap & Hold Magic
+
+        ButtonState tapHoldState = ButtonState.None;
+        float tapHoldTimer = 0;
+        float fastTapTimer = 0;
+
+        #endregion
+
+        void Update()
+        {
+            if (sfxCooldown > 0)
+                sfxCooldown -= Time.deltaTime;
+
+            SetSprite();
+
+            ButtonTapAndHold();
+        }
+
+        void SetSprite()
+        {
+            if (IsPressed())
+            {
+                if (interactState)
+                {
+                    if (enabledSpritePressed != null)
+                        image.sprite = enabledSpritePressed;
+                }
+                else
+                {
+                    if (disabledSpritePressed != null)
+                        image.sprite = disabledSpritePressed;
+                }
+            }
+            else
+            {
+                if (interactState)
+                {
+                    if (enabledSprite != null)
+                        image.sprite = enabledSprite;
+                }
+                else
+                {
+                    if (disabledSprite != null)
+                        image.sprite = disabledSprite;
+                }
+            }
+        }
+
+        void ButtonPress()
+        {
+            if (IsActive())
+            {
+                PlaySoundEffect();
+
+                if (interactState)
+                    OnClicked?.Invoke();
+                else
+                    OnDisabledClicked?.Invoke();
+            }
+        }
+
+        void ButtonTapAndHold()
+        {
+            if (tapAndHold)
+            {
+                if (IsPressed())
+                {
+                    float time = Time.unscaledDeltaTime;
+                    if (tapHoldState == ButtonState.None)
+                    {
+                        tapHoldTimer = initialTapDelay;
+                        tapHoldState = ButtonState.InitialTapDelay;
+                    }
+                    else if (tapHoldState == ButtonState.InitialTapDelay)
+                    {
+                        tapHoldTimer -= time;
+                        if (tapHoldTimer <= 0)
+                        {
+                            tapHoldState = ButtonState.HoldDelay;
+                            tapHoldTimer = holdDelay;
+                            fastTapTimer = fastTapDelay;
+
+                            ButtonPress();
+                        }
+                    }
+                    else if (tapHoldState == ButtonState.HoldDelay)
+                    {
+                        tapHoldTimer -= time;
+                        fastTapTimer -= time;
+
+                        while (tapHoldTimer <= 0)
+                        {
+                            ButtonPress();
+
+                            if (fastTapTimer <= 0)
+                                tapHoldTimer += fastTapHoldDelay;
+                            else
+                                tapHoldTimer += holdDelay;
+                        }
+                    }
+                }
+                else
+                    tapHoldState = ButtonState.None;
+            }
+        }
+
+        void PlaySoundEffect()
+        {
+            if (sfxCooldown <= 0)
+            {
+                if (this.IsActive())
+                {
+                    if (interactState)
+                    {
+                        if (enabledSound != null)
+                            PlayEffectSound(enabledSound);
+                    }
+                    else
+                    {
+                        if (disabledSound != null)
+                            PlayEffectSound(disabledSound);
+                    }
+
+                    sfxCooldown = soundEffectCooldown;
+                }
+            }
+        }
+
+        void PlayEffectSound(AudioClip clip)
+        {
+            if (UniversalAudioSource.instance != null)
+                UniversalAudioSource.instance.effectLayer.PlayEffect(clip);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (tapHoldState != ButtonState.HoldDelay)
+            {
+                ButtonPress();
+            }
+        }
+
+        public void SetHoldDelays(float tapsPerSecond)
+        {
+            holdDelay = Mathf.Max(1f / tapsPerSecond, 0.01f);
+            fastTapHoldDelay = Mathf.Max(1f / tapsPerSecond, 0.01f);
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            transition = Transition.None;
+            SetSprite();
+        }
+#endif
+
+
+#if UNITY_EDITOR
+        [MenuItem("GameObject/Convert/Button (UI)/Advanced Button", false, 7)]
+        static void ConvertToAdvancedButton(MenuCommand menuCommand)
+        {
+            GameObject go = menuCommand.context as GameObject;
+            Button button = go.GetComponent<Button>();
+
+            if (button != null)
+            {
+                // Temp Collect for Convert
+                bool interactable = button.interactable;
+                SpriteState spriteState = button.spriteState;
+                ColorBlock colors = button.colors;
+
+                GameObject holderObj = button.gameObject;
+
+                // Destroy old Button Component
+                DestroyImmediate(button);
+
+                AdvancedButton newButton = holderObj.AddComponent<AdvancedButton>();
+                newButton.interactable = interactable;
+                newButton.spriteState = spriteState;
+                newButton.colors = colors;
+            }
+        }
+
+        [MenuItem("GameObject/UI/Advanced Button")]
+        static void CreateAdvancedButton(MenuCommand menuCommand)
+        {
+            // Develope Main Object
+            GameObject go = new GameObject("Advanced Button", typeof(RectTransform), typeof(Image), typeof(AdvancedButton));
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+
+            go.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 32);
+            go.GetComponent<AdvancedButton>().image = go.GetComponent<Image>();
+
+
+            // Develope Child Object (Text)
+            GameObject child = new GameObject("Text", typeof(RectTransform), typeof(TextMeshProUGUI));
+            GameObjectUtility.SetParentAndAlign(child, go);
+
+            TextMeshProUGUI childText = child.GetComponent<TextMeshProUGUI>();
+            childText.text = "New Text";
+            childText.color = Color.black;
+            childText.alignment = TextAlignmentOptions.Center;
+
+            RectTransform childRect = child.GetComponent<RectTransform>();
+            childRect.anchorMax = new Vector2(1, 1);
+            childRect.anchorMin = new Vector2(0, 0);
+            childRect.sizeDelta = new Vector2(0, 0);
+
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Selection.activeObject = go;
+        }
+#endif
+    }
+
+}
---
+++ Runtime/Button/ButtonState.cs
@@ -0,0 +1,10 @@
+
+namespace Quartzified.UI
+{
+    public enum ButtonState
+    {
+        None = 0,
+        InitialTapDelay = 1,
+        HoldDelay = 2
+    }
+}
