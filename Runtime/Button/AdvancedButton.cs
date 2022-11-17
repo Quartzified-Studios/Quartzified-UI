900981d648f740ec833de5362a49287d86ed472f
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-11-17 12:20:37 +0100

    Buttons Simplified
    Added new Right Click Event

--- Runtime/Button/AdvancedButton.cs
+++ Runtime/Button/AdvancedButton.cs
@@ -1,119 +1,100 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine.Events;
-using Quartzified.Audio;
-
-#if UNITY_EDITOR
-using UnityEditor;
-using TMPro;
-#endif
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Quartzified.UI
 {
     [RequireComponent(typeof(RectTransform))]
     public class AdvancedButton : Selectable, IPointerClickHandler
     {
-        [Header("Visual")]
-        public Sprite enabledSprite;
-        public Sprite enabledSpritePressed;
-        [Space]
-        public Sprite disabledSprite;
-        public Sprite disabledSpritePressed;
-
-        [Header("Tap & Hold")]
+        [Header("Tap and Hold")]
+        [Tooltip("Enable Tap and Hold!\n(Only works with Left Click Events)")]
         public bool tapAndHold;
+
+        [Space]
+        [Tooltip("Time the button needs to be held for before initializing hold cycle.\n(Usually sooner than [Fast Tap Delay]")]
         public float initialTapDelay = 0;
+        [Tooltip("Time waited between every hold cycle.\n(Usually greater than [Fast Hold Delay]")]
         public float holdDelay = 0.5f;
-        public float fastTapDelay = 0.2f;
-        public float fastTapHoldDelay = 0.1f;
 
-        [Header("Sound Effects")]
-        public AudioClip enabledSound;
-        public AudioClip disabledSound;
-        public float soundEffectCooldown = .1f;
-        float sfxCooldown;
+        [Space]
+        [Tooltip("Time the button needs to be held for before initializing quick hold cycle.\n(Usually greater than [Initial Tap Delay]")]
+        public float fastTapDelay = 0.4f;
+        [Tooltip("Time waited between every quick hold cycle.\n(Usually lower than [Hold Delay]")]
+        public float fastHoldDelay = 0.2f;
 
-        [Header("Events")]
-        public UnityEvent OnClicked;
-        public UnityEvent OnDisabledClicked;
-        
-        // Enabled Click or Disabled Click : True / False
-        public bool interactState = true;
+        [Space]
+        public UnityEvent OnClick;
+        public UnityEvent OnRightClick;
 
-        #region Tap & Hold Magic
-
+        // Tap and Hold States
         ButtonState tapHoldState = ButtonState.None;
         float tapHoldTimer = 0;
         float fastTapTimer = 0;
 
-        #endregion
-
         void Update()
         {
-            if (sfxCooldown > 0)
-                sfxCooldown -= Time.deltaTime;
-
-            SetSprite();
-
-            ButtonTapAndHold();
+            HandleTapAndHold();
         }
 
-        void SetSprite()
+        public void PressLeft()
         {
-            if (IsPressed())
-            {
-                if (interactState)
-                {
-                    if (enabledSpritePressed != null)
-                        image.sprite = enabledSpritePressed;
-                }
-                else
-                {
-                    if (disabledSpritePressed != null)
-                        image.sprite = disabledSpritePressed;
-                }
-            }
-            else
-            {
-                if (interactState)
-                {
-                    if (enabledSprite != null)
-                        image.sprite = enabledSprite;
-                }
-                else
-                {
-                    if (disabledSprite != null)
-                        image.sprite = disabledSprite;
-                }
-            }
+
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            OnClick?.Invoke();
         }
 
-        void ButtonPress()
+        public void PressRight()
         {
-            if (IsActive())
-            {
-                PlaySoundEffect();
+            if (!IsActive() || !IsInteractable())
+                return;
 
-                if (interactState)
-                    OnClicked?.Invoke();
-                else
-                    OnDisabledClicked?.Invoke();
-            }
+            OnRightClick?.Invoke();
         }
 
-        void ButtonTapAndHold()
+        public virtual void OnPointerClick(PointerEventData eventData)
         {
+            // Check if we are currently in Hold State
+            // If we are we need to skip the input as it would register when letting go of the button
+            if (tapHoldState == ButtonState.HoldDelay)
+                return;
+
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                PressLeft();
+                return;
+            }
+
+            if (eventData.button == PointerEventData.InputButton.Right)
+            {
+                PressRight();
+                return;
+            }
+
+        }
+
+
+        void HandleTapAndHold()
+        {
+            // Check that we want to call hold Event
             if (tapAndHold)
             {
+                // Check that we are currently holding down the button
                 if (IsPressed())
                 {
+                    // Use Time.unscaledDeltaTime to not be affected by TimeScale
                     float time = Time.unscaledDeltaTime;
+
+                    // If Button State is None Trigger the Initial Tap Delay
                     if (tapHoldState == ButtonState.None)
                     {
                         tapHoldTimer = initialTapDelay;
                         tapHoldState = ButtonState.InitialTapDelay;
                     }
+                    // If Button State is InitialTapDelay wait out the first delay to Trigger Hold & Trigger First Press
                     else if (tapHoldState == ButtonState.InitialTapDelay)
                     {
                         tapHoldTimer -= time;
@@ -123,9 +104,10 @@ namespace Quartzified.UI
                             tapHoldTimer = holdDelay;
                             fastTapTimer = fastTapDelay;
 
-                            ButtonPress();
+                            PressLeft();
                         }
                     }
+                    // If Button State is HoldDelay cycle through the tap delays to Trigger Press
                     else if (tapHoldState == ButtonState.HoldDelay)
                     {
                         tapHoldTimer -= time;
@@ -133,128 +115,21 @@ namespace Quartzified.UI
 
                         while (tapHoldTimer <= 0)
                         {
-                            ButtonPress();
+                            PressLeft();
 
                             if (fastTapTimer <= 0)
-                                tapHoldTimer += fastTapHoldDelay;
+                                tapHoldTimer += fastHoldDelay;
                             else
                                 tapHoldTimer += holdDelay;
                         }
                     }
                 }
                 else
-                    tapHoldState = ButtonState.None;
-            }
-        }
-
-        void PlaySoundEffect()
-        {
-            if (sfxCooldown <= 0)
-            {
-                if (this.IsActive())
                 {
-                    if (interactState)
-                    {
-                        if (enabledSound != null)
-                            PlayEffectSound(enabledSound);
-                    }
-                    else
-                    {
-                        if (disabledSound != null)
-                            PlayEffectSound(disabledSound);
-                    }
-
-                    sfxCooldown = soundEffectCooldown;
+                    tapHoldState = ButtonState.None;
                 }
             }
+
         }
-
-        void PlayEffectSound(AudioClip clip)
-        {
-            if (UniversalAudioSource.instance != null)
-                UniversalAudioSource.instance.effectLayer.PlayEffect(clip);
-        }
-
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            if (tapHoldState != ButtonState.HoldDelay)
-            {
-                ButtonPress();
-            }
-        }
-
-        public void SetHoldDelays(float tapsPerSecond)
-        {
-            holdDelay = Mathf.Max(1f / tapsPerSecond, 0.01f);
-            fastTapHoldDelay = Mathf.Max(1f / tapsPerSecond, 0.01f);
-        }
-
-#if UNITY_EDITOR
-        protected override void OnValidate()
-        {
-            base.OnValidate();
-
-            transition = Transition.None;
-            SetSprite();
-        }
-#endif
-
-
-#if UNITY_EDITOR
-        [MenuItem("GameObject/Convert/Button (UI)/Advanced Button", false, 7)]
-        static void ConvertToAdvancedButton(MenuCommand menuCommand)
-        {
-            GameObject go = menuCommand.context as GameObject;
-            Button button = go.GetComponent<Button>();
-
-            if (button != null)
-            {
-                // Temp Collect for Convert
-                bool interactable = button.interactable;
-                SpriteState spriteState = button.spriteState;
-                ColorBlock colors = button.colors;
-
-                GameObject holderObj = button.gameObject;
-
-                // Destroy old Button Component
-                DestroyImmediate(button);
-
-                AdvancedButton newButton = holderObj.AddComponent<AdvancedButton>();
-                newButton.interactable = interactable;
-                newButton.spriteState = spriteState;
-                newButton.colors = colors;
-            }
-        }
-
-        [MenuItem("GameObject/UI/Advanced Button")]
-        static void CreateAdvancedButton(MenuCommand menuCommand)
-        {
-            // Develope Main Object
-            GameObject go = new GameObject("Advanced Button", typeof(RectTransform), typeof(Image), typeof(AdvancedButton));
-            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
-
-            go.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 32);
-            go.GetComponent<AdvancedButton>().image = go.GetComponent<Image>();
-
-
-            // Develope Child Object (Text)
-            GameObject child = new GameObject("Text", typeof(RectTransform), typeof(TextMeshProUGUI));
-            GameObjectUtility.SetParentAndAlign(child, go);
-
-            TextMeshProUGUI childText = child.GetComponent<TextMeshProUGUI>();
-            childText.text = "New Text";
-            childText.color = Color.black;
-            childText.alignment = TextAlignmentOptions.Center;
-
-            RectTransform childRect = child.GetComponent<RectTransform>();
-            childRect.anchorMax = new Vector2(1, 1);
-            childRect.anchorMin = new Vector2(0, 0);
-            childRect.sizeDelta = new Vector2(0, 0);
-
-            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
-            Selection.activeObject = go;
-        }
-#endif
     }
-
 }
